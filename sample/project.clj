49119e157908716fb7fab95ca31c6f31e406d303
a-murphy <a-murphy@users.noreply.github.com>
2018-02-21 16:47:37 -0800

    Update project.clj

--- sample/project.clj
+++ sample/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :profiles {:dev {:plugins [[test2junit "1.1.1"]]}}
   :main ^:skip-aot sample.core
   :target-path "target/%s"
